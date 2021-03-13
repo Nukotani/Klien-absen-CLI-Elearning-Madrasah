e5e9e9f90bfc449e51362d2f5abbd825318ebfea
fatihsatrio <fatihsatrio@tfwno.gf>
2021-03-13 12:33:37 +0700

    removed some unnecessary header files

--- src/include.h
+++ src/include.h
@@ -1,14 +1,11 @@
 #pragma once
-#include <stdbool.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <stdlib.h>
 #include <regex.h>
 #include <time.h>
-#define ELEARING "http://elearning.man1balam.sch.id"
-#define PORT 80
+
+typedef enum {false, true} bool;
